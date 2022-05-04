d726ad5dd6d04625a3b3f716eb056e56e05c4c9b
Patrick Coxall <mr.coxall@mths.ca>
2022-05-04 10:00:29 -0400

    update

--- setup.sh
+++ setup.sh
@@ -55,6 +55,12 @@ sudo apt install build-essential -y
 # install C++ linter
 pip3 install cpplint
 
+# load GitHub CLI
+echo load GitHub CLI
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update
+sudo apt install gh -y
 
 # then remove the dot_files directory 
 # sudo rm -R ~/AWS-Ubuntu-Setup-Script
